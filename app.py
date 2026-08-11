2890fd58b0c711d742c5e6c068a1900aca3e64b5
juansebastianrc30-cmyk <juansebastianrc30@gmail.com>
2026-08-10 21:49:49 -0500

    Se agrego

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
 from flask import Flask
-
-from config import Config
-from database.conexion import init_db
-from routes.clientes_routes import clientes_bp
-
-app = Flask(__name__)
-app.config.from_object(Config)
-
-init_db()
-
-app.register_blueprint(clientes_bp)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+app = Flask(__name__) 
+@app.route('/')
+def inicio(): 
+    return render_template('index.html')
+    if __name__ == '__main__': 
+        app.run(debug=True) 
--- config.py
+++ config.py
@@ -2,10 +2,7 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'isailo-maps-secret-key-2026')
-
-    DB_HOST = os.environ.get('DB_HOST', 'localhost')
-    DB_PORT = int(os.environ.get('DB_PORT', 3306))
-    DB_USER = os.environ.get('DB_USER', 'root')
-    DB_PASSWORD = os.environ.get('DB_PASSWORD', '')
-    DB_NAME = os.environ.get('DB_NAME', 'isailo_maps')
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = 'isailo_maps'
