0730b398fda961b8269e065b3b5d6003aaebb2ca
Trimau <pandajsbc@outlook.es>
2026-08-18 20:57:02 -0500

    elementos comments para app.py e index(creo)

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+#'llamando librerias'
 from flask import Flask, render_template, request
 from config import Config
 from database.conexion import mysql
-
+#llama flask
 app = Flask(__name__)
 
 # Configuración MySQL
@@ -13,29 +14,39 @@ app.config['MYSQL_DB'] = Config.MYSQL_DB
 # Inicializar MySQL
 mysql.init_app(app)
 
+#por defecto abre index.html
 @app.route('/')
 def inicio():
     return render_template('index.html')
-
-@app.route('/register', methods=['GET'])
-def register():
+#cuando se abre el href, /register_pag, abre el html
+@app.route('/register_pag')
+def register_pag():
     return render_template('register.html')
 
-@app.route('/register', methods=['POST'])
+#FORMULARIO, de action='/register', metodos , como pull push
+@app.route('/register', methods=['GET', 'POST'])
 def guardar_usuario():
-    nombre = request.form.get('nombre', '')
-    apellido = request.form.get('apellido', '')
-    correo = request.form.get('correo', '')
-    password_hash = request.form.get('password_hash', '')
+    if request.method == 'POST':
+        #post= enviar info al server
 
-    cursor = mysql.connection.cursor()
-    sql = """
-    INSERT INTO usuarios(nombre, apellido, correo, password_hash)
-    VALUES(%s, %s, %s, %s)
-    """
-    datos = (nombre, apellido, correo, password_hash)
-    cursor.execute(sql, datos)
-    mysql.connection.commit()
-    cursor.close()
+        nombre = request.form.get('nombre', '') #nombre(variable) <-- nombre(html)
+        apellido = request.form.get('apellido', '')
+        correo = request.form.get('correo', '')
+        password_hash = request.form.get('password_hash', '')
 
-    return "Cliente guardado correctamente"
+        cursor = mysql.connection.cursor() #deja que python maneje sql/guarda en sql(var) las intrucciones
+        sql = """ 
+        INSERT INTO usuarios(nombre, apellido, correo, password_hash) 
+        VALUES(%s, %s, %s, %s)
+        """
+        datos = (nombre, apellido, correo, password_hash) #coge todos en datos
+        cursor.execute(sql, datos) #ejecutar el insert into
+        mysql.connection.commit() # confirmar
+        cursor.close() #CIERRA la herramienta
+
+        return "Cliente guardado correctamente" # mensaje despues
+
+    return render_template('register.html') #opcional, renderizar
+
+if __name__ == '__main__':
+    app.run(debug=True) #ejecuta flask con app.py
